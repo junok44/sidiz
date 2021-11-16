fc355daecc3ac0c165cb1b5f624df92e6cd2d60e
junok <89637844+junok44@users.noreply.github.com>
2021-11-16 10:42:19 +0900

    JQUERY 추가

--- js/script.js
+++ js/script.js
@@ -1,25 +1,51 @@
 // html 이 준비가 되면 실행한다.
-$(document).ready(function() {
-      // 모달창 기능 
-    let modal = $('.modal');    
+$(document).ready(function () {
+    // 주메뉴
+    let menu_about = $('.menu-about');
+    let depth1 = $('.depth1');
+    let product = $('.product');
+    let depth2 = $('.depth2');
+    let support = $('.support');
+    let depth3 = $('.depth3');
+    menu_about.mouseenter(function () {
+        depth1.show();
+    });
+    menu_about.mouseleave(function () {
+        depth1.hide();
+    });
+    product.mouseenter(function () {
+        depth2.show();
+    });
+    product.mouseleave(function () {
+        depth2.hide();
+    });
+    support.mouseenter(function () {
+        depth3.show();
+    });
+    support.mouseleave(function () {
+        depth3.hide();
+    });
+
+    // 모달창 기능 
+    let modal = $('.modal');
     let modal_cont = $('.modal-cont');
     let modal_close = $('.modal-close');
     modal.fadeIn();
-    modal_close.click(function(){
+    modal_close.click(function () {
         modal.fadeOut();
     });
-    modal.click(function(){
+    modal.click(function () {
         modal.fadeOut();
     });
-    modal_cont.click(function(event) {
+    modal_cont.click(function (event) {
         event.stopPropagation();
     });
     // 탭메뉴
-    let all_product =$('.all-product');
+    let all_product = $('.all-product');
     let more_gnb = $('.more-gnb');
 
-    $.each(all_product, function(index, item) {
-        $(this).mouseenter(function(){
+    $.each(all_product, function (index, item) {
+        $(this).mouseenter(function () {
             $(this).find('.more-gnb').show();
         });
     });
@@ -35,5 +61,9 @@ $(document).ready(function() {
             prevEl: ".sw-visual-prev",
         },
         effect: "fade",
+        fadeEffect: {
+            crossFade: true
+        },
     });
+
 });
